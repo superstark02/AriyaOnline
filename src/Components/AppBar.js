60264fbe290050984e0b6c0c70c8f42a0a6874ff
superstark02 <superstark02@gmail.com>
2020-08-16 13:06:20 +0530

    8/16/2020

---
+++ src/Components/AppBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AppBar extends Component {
+    render() {
+        return (
+            <div>
+                <div className="app-bar-blur" ></div>
+            </div>
+        )
+    }
+}
+
+export default AppBar
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import HomeHeader from '../Components/Home/HomeHeader'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <HomeHeader/>
+            </div>
+        )
+    }
+}
+
+export default Home
